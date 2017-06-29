b99a1ae3c8e7537d19e369fc4525a2e961c40b64
deepglint <deepglint@EDZ015.local>
2017-06-29 20:56:32 +0800

    start

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+	devtool: false,
+	entry: [
+		"webpack-dev-server/client?http://localhost:8080",
+  		'webpack/hot/only-dev-server',
+		"./app/index.js"
+	],
+	output: {
+		path: path.join(__dirname, 'dest'),
+		publicPath: "/dest/",
+		filename: "bundle.js"
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.jsx?$/,
+				loader: 'babel-loader',
+				exclude: /(node_modules|bower_components)/,
+				query: {
+					presets:['react', 'es2015']
+				}
+			}
+		]
+	},
+	plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ]
+}
